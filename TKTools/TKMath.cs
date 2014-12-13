1e4330866b617a3a036b0150e45af191216a48cf
DuckMonster <emil.strm@gmail.com>
2014-12-13 16:23:14 +0100

    Math methods added

---
+++ TKTools/TKMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenTK;
+
+namespace TKTools
+{
+	public static class TKMath
+	{
+		public static float GetAngle(Vector2 a, Vector2 b)
+		{
+			return GetAngle(b - a);
+		}
+		public static float GetAngle(Vector2 vec)
+		{
+			return (float)Math.Atan2(vec.Y, vec.X);
+		}
+
+		public static Vector2 GetAngleVector(float a)
+		{
+			return new Vector2((float)Math.Cos(ToRadians(a)), (float)Math.Sin(ToRadians(a)));
+		}
+
+		public static float ToRadians(float deg)
+		{
+			return (float)(deg / 180 * Math.PI);
+		}
+
+		public static float ToDegrees(float rad)
+		{
+			return (float)(rad * 180 / Math.PI);
+		}
+
+		public static float Exp(float v, float max)
+		{
+			return (float)Math.Exp(-v * max);
+		}
+	}
+}
